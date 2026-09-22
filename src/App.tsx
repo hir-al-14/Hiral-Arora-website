2eb903225a86b80fffc4e17f6164b0efea399cbd
Hiral <hiral.arora1406@gmail.com>
2026-09-22 14:18:39 -0700

    add dark color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }, []);
+
   return (
     <div className="site">
       <div className="page-content">
